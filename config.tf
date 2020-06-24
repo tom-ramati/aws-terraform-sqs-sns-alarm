9b48c6e55fb84e2a4d2eef20f362b27fc2b6ac8f
tom <tomikruso@gmail.com>
2020-06-24 18:23:06 +0200

    Replacing local modules path to git path

--- config.tf
+++ config.tf
@@ -1,13 +1,13 @@
 # team 1
 
 module "team1-queue1" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_makelaars"
   messages = 10
 }
 
 module "team1-queue2" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_makelaars_errors"
   messages = 1
 }
@@ -15,40 +15,40 @@ module "team1-queue2" {
 # team 2
 
 module "team2-queue1" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_new_houses"
   messages = 10
 }
 
-/*
+
 module "team2-queue2" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_new_houses_errors"
   messages = 1
 }
-*/
+
 module "team2-queue3" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_edited_houses"
   messages = 1
   notify   = false
 }
 
 module "team2-queue4" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_edited_houses_errors"
   messages = 1
 }
 
 module "team2-queue5" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_removed_houses"
   messages = 1
   notify   = false
 }
 
 module "team2-queue6" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_removed_houses_errors"
   messages = 1
 }
@@ -56,25 +56,25 @@ module "team2-queue6" {
 # team 3
 
 module "team3-queue1" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_stats_phone_clicks"
   messages = 25
 }
 
 module "team3-queue2" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_stats_phone_clicks_errors"
   messages = 1
 }
 
 module "team3-queue3" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_stats_facebook_clicks"
   messages = 25
 }
 
 module "team3-queue4" {
-  source   = "./modules"
+  source   = "github.com/tom-ramati/aws-terraform-sqs-sns-alarm/modules/"
   name     = "test_devops_stats_facebook_clicks_errors"
   messages = 1
 }
